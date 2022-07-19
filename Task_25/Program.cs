181e8ad2ff1eef8a0e5d99ee4a3ec2a582180ad9
Yuriy Novikov <7413856@mail.ru>
2022-07-20 01:05:10 +0300

    задача 25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,3 +3,10 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.WriteLine("Возвдение числа А в натуральную степень В");
+Console.Write("Ввидите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввидите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = Convert.ToInt32(Math.Pow (a, b)); 
+Console.Write($"Число {a} в степени {b} -> {result}");
