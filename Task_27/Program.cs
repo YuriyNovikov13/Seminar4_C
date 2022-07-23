7614ede68e8260ab171d36b966e813148b93456a
Yuriy Novikov <7413856@mail.ru>
2022-07-23 19:41:54 +0300

    задача 27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -5,16 +5,16 @@
 Console.WriteLine("Определение суммы цифр в числе");
 Console.Write("Ввидите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int Number(int number)
+int Sum(int number)
 {
-    int exp = 0;
+    int sum = 0;
     int i = number;
     while (i != 0)
     {
-        exp = exp + i % 10;
+        sum = sum + i % 10;
         i = i / 10;
     }
-    return exp;
+    return sum;
 }
 int result = Number(num);
 Console.Write($"сумма цифр числа {num}, равна {result}");
